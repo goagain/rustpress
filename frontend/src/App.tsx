66a40b2632ea061b57d93f475b352f17223c01a4
goagain <remain.goagain@gmail.com>
2025-12-22 23:08:22 -0800

    Init

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+export function App() {
+  return (
+    <div className="p-4">
+      <p className="text-gray-700">
+        React runtime mounted successfully.
+      </p>
+    </div>
+  );
+}
+
+export default App;
